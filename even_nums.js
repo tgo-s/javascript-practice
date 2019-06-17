455e7f0897f6f2e398e73cc399735179103c8e8e
Thiago Simionato dos Santos <tgo.simionato@gmail.com>
2019-06-17 16:54:38 -0300

    Even nums solution candidate

--- even_nums.js
+++ even_nums.js
@@ -1,40 +1,63 @@
-/**
+/*
 What is wrong with the code?
 
-It should only collect those numbers that are even, however if it is collecting all numbers (even the odd ones).
+It should only collect those numbers that are even, however if it is collecting all numbers
+(even the odd ones).
 
 Fix it to collect all the even numbers.
 
 Input will be numbers, separated by spaces.
- */
+*/
 
- //Installed node modules: jquery underscore request express jade shelljs passport http sys lodash async mocha moment connect validator restify ejs ws co when helmet wrench brain mustache should backbone forever debug
+// Installed node modules: jquery underscore request express jade shelljs passport http sys lodash
+// async mocha moment connect validator restify ejs ws co when helmet wrench brain mustache should
+// backbone forever debug
+process.stdout.write('Even program. \n');
+process.stdout.write('Please enter with integer numbers separated by spaces. \n');
+process.stdout.write('Or press [Ctrl + D] at anytime to end program. \n');
+
+const inputRegex = new RegExp(/^-?\d*$/);
+const evenNumbers = [];
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
-var util = require('util');
-var rawInput = "";
 
-process.stdin.on('data', function (text) {
-    rawInput += text;
-});
-
-
-var evenNumbers = [];
-
-process.stdin.on('end', function () {
-    var inputs = rawInput.split(' ');    
-    
-    inputs.forEach(function (n) {
-        addEvensOnly(n);            
-    });
-    console.log('even numbers:', JSON.stringify(evenNumbers));
-});
-//Edit above at your own risk
-
-//TODO: Fix this function
 function addEvensOnly(num) {
-    if (num % 2 === 0)
-        console.log(num, 'is even, adding!');
-        evenNumbers.push(num);
+  if (num % 2 === 0) {
+    process.stdout.write(`${num} is even, adding! \n`);
+    evenNumbers.push(num);
+  }
 }
+
+function testInputValue(value) {
+  if (typeof value === 'number' || inputRegex.test(value)) {
+    return true;
+  }
+  return false;
+}
+
+
+function inputEvenValidation(text) {
+  let msg = '';
+  try {
+    const inputVal = text.replace(/\r?\n|\r/g, ' ').trim();
+    const inputArr = inputVal.split(' ');
+    const allNumbers = inputArr.every(e => testInputValue(e));
+    if (allNumbers) {
+      inputArr.forEach(v => addEvensOnly(v));
+    } else {
+      msg = 'One or more item of array is not a number. Please enter with integer numbers separated by spaces \n';
+    }
+  } catch (e) {
+    msg = 'An error occured trying to validate your input. Please verify if you entered a valid array. \n';
+  }
+  if (msg && msg !== '') { process.stdout.write(msg); }
+}
+
+process.stdin.on('data', (text) => {
+  inputEvenValidation(text);
+});
+
+process.stdin.on('end', () => {
+  if (evenNumbers.length > 0) process.stdout.write(`even numbers: ${JSON.stringify(evenNumbers)}`);
+});
