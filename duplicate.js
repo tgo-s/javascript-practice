5fe20dbdbbc3b36ac2e497f496671b476af22854
Thiago Simionato dos Santos <tgo.simionato@gmail.com>
2019-06-17 15:45:22 -0300

    Modification on duplicate logic. Now it only accept an array as input parameter

--- duplicate.js
+++ duplicate.js
@@ -10,39 +10,39 @@ Input will always be an array.
 // async mocha moment connect validator restify ejs ws co when helmet wrench brain mustache should
 // backbone forever debug
 process.stdout.write('Duplicate program. \n');
-process.stdout.write('Please enter an integer. \n');
+process.stdout.write('Please enter an array. \n');
 process.stdout.write('Or press [Ctrl + D] at anytime to end program. \n');
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
 const tempArray = [];
-const inputRegex = new RegExp(/^-?\d*$/);
 
 function duplicate(arr) {
   return arr.concat(arr);
 }
 
-function inputIntegerValidation(text) {
+function inputValidation(text) {
   let msg = '';
   try {
-    const inputVal = text.replace(/\r?\n|\r/g, ' ').trim();
-
-    if (inputRegex.test(inputVal)) {
-      const value = parseInt(inputVal, 10);
-      if (Number.isInteger(value)) { tempArray.push(value); }
+    const usrInputArr = JSON.parse(text);
+    const isArray = Array.isArray(usrInputArr);
+    if (isArray) {
+      usrInputArr.forEach((el) => {
+        tempArray.push(el);
+      });
     } else {
-      msg = 'Input entered is not a valid integer. Only integer numbers are accepted. Please try again. \n';
+      msg = 'Input entered is not a valid array. Only array input are accepted. Please try again. \n';
     }
   } catch (e) {
-    msg = 'An error occured trying to validate your input. Please verify if you entered a valid integer. \n';
-    console.log(`Exception: ${e}`);
+    msg = 'An error occured trying to validate your input. Please verify if you entered a valid array. (E.g: [1,2,3,4]) \n';
+    // console.log(`Exception: ${e}`);
   }
   if (msg && msg !== '') { process.stdout.write(msg); }
 }
 
 process.stdin.on('data', (text) => {
-  inputIntegerValidation(text);
+  inputValidation(text);
 });
 
 process.stdin.on('end', () => {
